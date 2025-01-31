07db53088d278a0623a92884a048f580c09d4659
Sussego <ccedu.uenp@gmail.com>
2025-01-31 03:23:40 -0300

    Divisão e Resto

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -15,9 +15,21 @@ def multiplicacao(a, b):
         return soma(a, multiplicacao(a, b - 1))
     return -multiplicacao(a, -b)
 
+def divisao(a, b):
+    if a < b:
+        return 0
+    return soma(1, divisao(subtracao(a, b), b))
+
+def resto_divisao(a, b):
+    if a < b:
+        return a
+    return resto_divisao(subtracao(a, b), b)
+
 
 
 # Testes
 print("Soma: ", soma(5, 3)) 
 print("Subtração: ", subtracao(10, 4)) 
 print("Multiplicação: ", multiplicacao(2, 4))
+print("Divisão: ", divisao(7, 2))
+print("Resto da divisão: ", resto_divisao(7, 2))

3bd3ae2b2ce5e467473989ef0b311f4c113a6852
Sussego <ccedu.uenp@gmail.com>
2025-01-31 03:16:31 -0300

    Multiplicação

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -8,8 +8,16 @@ def subtracao(a, b):
         return a
     return subtracao(a - 1, b - 1)
 
+def multiplicacao(a, b):
+    if b == 0:
+        return 0
+    if b > 0:
+        return soma(a, multiplicacao(a, b - 1))
+    return -multiplicacao(a, -b)
+
 
 
 # Testes
-print("Soma: ", soma(5, 3))  # 8
-print("Subtração: ", subtracao(10, 4))  # 6
+print("Soma: ", soma(5, 3)) 
+print("Subtração: ", subtracao(10, 4)) 
+print("Multiplicação: ", multiplicacao(2, 4))
